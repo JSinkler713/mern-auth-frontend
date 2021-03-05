edca141d04da98be2bfd8e7f7658290667cdf00f
James Sinkler <jamessinkler713@gmail.com>
2021-03-05 09:38:38 -0800

    commit form j

--- src/utils/setAuthToken.js
+++ src/utils/setAuthToken.js
@@ -4,7 +4,14 @@ import axios from 'axios';
 // Any routes that are protected will require the JWT in order to access them.
 
 const setAuthToken = (token) => {
+  if (token) {
+    axios.defaults.headers.common['Authorization'] = token;
+    console.log('the headers with axios from utils'
+    console.log(axios.defaults.headers.common);
     // Add code here
+  } else {
+      delete axios.defaults.headers.common['Authorization']
+  }
 }
 
 export default setAuthToken;
